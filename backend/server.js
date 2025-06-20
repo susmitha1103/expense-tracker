aee1df008487dd42f38de87968f7f88d1cadb97e
Susmitha Reddy <susmitha.me@hotmail.com>
2025-06-21 02:10:33 +0530

    chore: enable cors in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
@@ -9,6 +10,12 @@ connectDB();
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
 
 app.use('/api/users', userRoutes);
 app.use('/api/expenses', expenseRoutes);
