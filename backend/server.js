6c522212a4b3f9d67e140fee5e62e444bbfa6f7a
Susmitha Reddy <susmitha.me@hotmail.com>
2025-07-28 00:33:17 +0530

    fix error

--- backend/server.js
+++ backend/server.js
@@ -11,15 +11,15 @@ connectDB();
 
 const app = express();
 
+
+app.use(express.json());
 app.use(cors({
   origin: ['http://localhost:5173',
-     'https://expense-tracker-lac-nu.vercel.app/'],
+     'https://expense-tracker-lac-nu.vercel.app'],
   credentials: true,
 }));
 
 app.options('*', cors());
-
-app.use(express.json());
 
 
 
