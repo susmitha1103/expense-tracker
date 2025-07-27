665bcf79234f4f60c3bcf0b38c18c313bc097e3f
Susmitha Reddy <susmitha.me@hotmail.com>
2025-07-28 00:53:12 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,6 @@ app.use(express.json());
 app.use(cors({
   origin: ['http://localhost:5173',
      'https://expense-tracker-lac-nu.vercel.app'],
-  credentials: true,
 }));
 
 app.options('*', cors());
